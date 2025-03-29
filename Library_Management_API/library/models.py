af1a4c5bc17527fe0ff4624a1a716216662ebc26
AbdulRahman Okasha <a.majdy41@gmail.com>
2025-03-29 04:14:39 +0200

    Add new fields to the transaction model to support overdue penalty

--- Library_Management_API/library/models.py
+++ Library_Management_API/library/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+from django.utils.timezone import now, timedelta
 
 
 class Book(models.Model):
@@ -8,82 +8,61 @@ class Book(models.Model):
     author = models.CharField(max_length=255)
     isbn = models.CharField(max_length=13, unique=True)
     published_date = models.DateField(null=True, blank=True, default=None)
+    total_copies = models.IntegerField()
     balance = models.PositiveIntegerField()
 
     def __str__(self):
         return f'{self.title} by {self.author}'
 
 
-class CustomUserManager(BaseUserManager):
+class CustomUser(AbstractUser):
 
-    def create_user(self, username, email, password=None, **extra_fields):
-        email = self.normalize_email(email)
-        user = self.model(username=username, email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, email, password=None, **extra_fields):
-        if not password:
-            raise ValueError('Superuser must have a password')
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('active_status', True)
-        return self.create_user(username, email, password=None, **extra_fields)
-
-
-class CustomeUserModel(AbstractBaseUser):
-    # Required Fields
-    username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(unique=True)
 
-    # Date of membership
     date_of_membership = models.DateTimeField(auto_now_add=True)
 
-    # Active status
     active_status = models.BooleanField(default=True)
 
-    # Additional fields for Django's admin functionality
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-
-    # specify the field used to login
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email']
-
-    # Custom manager
-    objects = CustomUserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
-        return self.username
-
-    def has_perm(self, perm, obj=None):
-        return self.is_staff
+        return self.email
 
 
-class Transactions(models.Model):
-    book = models.ForeignKey(
-        Book, on_delete=models.CASCADE, related_name='transactions')
-    user = models.ForeignKey(
-        CustomeUserModel, on_delete=models.CASCADE, related_name='transactions')
-    TRANSACTION_CHOICES = [
-        ('return', 'Return'),
-        ('check_out', 'Check_out')
-    ]
-    transaction_type = models.CharField(
-        max_length=10, choices=TRANSACTION_CHOICES)
-    transaction_date = models.DateTimeField(auto_now_add=True)
-    due_date = models.DateTimeField()
-    return_date = models.DateTimeField(null=True, blank=True)
+class Transaction(models.Model):
     STATUS_CHOICES = [
         ('checked_out', 'Checked out'),
         ('returned', 'Returned'),
         ('over_due', 'Overdue')
     ]
-    status = models.CharField(max_length=15, choices=STATUS_CHOICES)
+    TRANSACTION_CHOICES = [
+        ('return', 'Return'),
+        ('check_out', 'Check_out')
+    ]
+    book = models.ForeignKey(
+        Book, on_delete=models.CASCADE, related_name='transactions')
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name='transactions')
+
+    transaction_type = models.CharField(
+        max_length=10, choices=TRANSACTION_CHOICES)
+    transaction_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(null=True, blank=True)
+    return_date = models.DateTimeField(null=True, blank=True)
+    status = models.CharField(
+        max_length=15, choices=STATUS_CHOICES, default='checked_out')
 
     class Meta:
         verbose_name = 'Transaction'
         verbose_name_plural = 'Transactions'
         ordering = ['-transaction_date']
-        unique_together = ['user', 'book']
+
+    def save(self, *args, **kwargs):
+        if self.transaction_type == 'check_out' and not self.due_date:
+            self.due_date = now() + timedelta(days=14)
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.book.title} {self.transaction_type}'
